74301f21a6c1fa56bf03bf7d954c49c8227886b1
ygorcanalli <ygorcanalli@gmail.com>
2014-04-18 18:51:04 -0300

    print output to a file

--- RocumentHandler/model/__init__.py
+++ RocumentHandler/model/__init__.py
@@ -1,5 +1,12 @@
 from model.content import Document
 
 documentStr = str(open("input.txt", "r").read())
+
+
 document = Document(documentStr)
-print document.__str__()
+
+outputStr = document.__str__()
+print outputStr
+
+output = open("output.txt", "w")
+output.write(outputStr)
