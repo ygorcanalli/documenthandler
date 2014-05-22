256bfd7939903664605414be24857f6b4f20d700
ygorcanalli <ygorcanalli@gmail.com>
2014-05-22 15:49:51 -0300

    C libary calling test with ctype

---
+++ DocumentHandler/sandbox/c_calling/add.c
@@ -0,0 +1,6 @@
+/* Add two numbers together and return the result */
+float
+add(float f1, float f2)
+{
+    return f1 + f2;
+}
---
+++ DocumentHandler/sandbox/c_calling/avg_array.c
@@ -0,0 +1,13 @@
+/* Take an array of floats, and return the their average */
+float
+avg_array(float *array, int num_entries)
+{
+    int i;
+    float sum = 0.0f;
+
+    for (i = 0; i < num_entries; i++)
+    {
+        sum += array[i];
+    }
+    return sum / num_entries;
+}
