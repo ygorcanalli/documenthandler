148282e0f59b1f76bc8d6710dc57c031097e42e0
ygorcanalli <ygor.canalli@gmail.com>
2014-12-10 19:18:09 -0200

    List of paragraphs and bag of paragraphs ok (hierarchically)

--- DocumentHandler/src/compare/structural_similarity.py
+++ DocumentHandler/src/compare/structural_similarity.py
@@ -4,7 +4,9 @@ Created on Nov 29, 2014
 @author: ygor
 '''
 
-from alignment import *
+from src.model.container import Bag, Set
+from src.compare.alignment import align_words, align_sentences, align_paragraphs, cos_similarity, normalized_sequential_levenshtein
+import numpy as np
 from pprint import pprint
 
 
@@ -20,8 +22,75 @@ def bag_of_sentences(s_content, t_content):
 def list_of_sentences(s_content, t_content):
     return align_sentences(s_content, t_content, normalized_sequential_levenshtein)
 
-def bag_of_paragraphs(s_content, t_content):
-    return align_sentences(s_content, t_content, cos_similarity)
+def bag_of_paragraphs(s_content, t_content, granule_alignment_funcion, threshold):
+    if granule_alignment_funcion == None and threshold == None:
+        return align_paragraphs(s_content, t_content, cos_similarity)
+    else:
+        # Get the paragraphs
+        s_itens = s_content.get_paragraphs()
+        t_itens = t_content.get_paragraphs()
+        
+        s_bag = s_itens.to_bag()
+        t_bag = t_itens.to_bag()
+        
+        s_set = s_itens.to_set()
+        t_set = t_itens.to_set()
+        
+        keys = s_set.union(t_set)
+        
+        array_s = np.zeros(len(keys))
+        array_t = np.zeros(len(keys))
+        
+        s_similarity_bucket = {}
+        t_similarity_bucket = {}
+        
+        for s_item in s_bag:            
+            for t_item in t_bag: 
+                
+                # if itens are differents, measure similarity
+                if (s_item != t_item):
+                    
+                    # similarity measurement
+                    simil = granule_alignment_funcion(s_item, t_item)
+                          
+                    # Verify equality by threshold and build the buckets
+                    if simil >= threshold:
+                        if s_similarity_bucket.has_key(s_item):
+                            # make union
+                            s_similarity_bucket[s_item].add(t_item)
+                        else: 
+                            # initialize bucket
+                            s_similarity_bucket[s_item] = Set([t_item])
+                    
+                        if t_similarity_bucket.has_key(t_item):
+                            # make union
+                            t_similarity_bucket[t_item].add(s_item)
+                        else:
+                            # initialize bucket
+                            t_similarity_bucket[t_item] = Set([s_item]) 
+                            
+        
+        
+        i = 0
+        for k in keys:
+            if s_bag.has_key(k):
+                array_s[i] = s_bag.get(k)
+            else:
+                if t_similarity_bucket.has_key(k):
+                    for b in t_similarity_bucket[k]:
+                        array_s[i] += s_bag.get(b)
+            if t_bag.has_key(k):
+                array_t[i] = t_bag.get(k)
+            else:       
+                if s_similarity_bucket.has_key(k):
+                    for b in s_similarity_bucket[k]:
+                        array_t[i] += t_bag.get(b)
+            i = i + 1
+                               
+        dotted = np.dot(array_s, np.transpose(array_t))
+        norms = np.linalg.norm(array_s) * np.linalg.norm(array_t)
+
+        return dotted/norms
 
 def list_of_paragraphs(s_content, t_content, granule_alignment_funcion, threshold):
     if granule_alignment_funcion == None and threshold == None:
@@ -33,11 +102,9 @@ def list_of_paragraphs(s_content, t_content, granule_alignment_funcion, threshol
     
         s_itens_set = s_itens.to_set()
         t_itens_set = t_itens.to_set()
-
     
         itens_equality = {}
-
-    
+  
         for s_item in s_itens_set:
             for t_item in t_itens_set:
                 
--- DocumentHandler/src/cos_test.py
+++ DocumentHandler/src/cos_test.py
@@ -15,15 +15,9 @@ from src.compare.structural_similarity import *
 t = document_from_pkl("../instances/hierarchical_test/pickled/genesis1.pkl")
 s = document_from_pkl("../instances/hierarchical_test/pickled/copy-of-genesis1.pkl")
 
-s_words_hash = s.get_words().to_hash_list()
-t_words_hash = t.get_words().to_hash_list()
-
-print list_of_paragraphs(s, t, threshold=None, granule_alignment_funcion=None)
-print list_of_paragraphs(s, t, threshold=0.8, granule_alignment_funcion=bag_of_words)
-print list_of_paragraphs(s, t, threshold=0.8, granule_alignment_funcion=list_of_words)
-#%%
-#print "Cosine similarity: %f" % cos_similarity(s_words_hash, t_words_hash)
-
-#print "Jaccard similarity: %f" % jaccard_similarity(s_words_hash, t_words_hash)
-
-#%%
+print "List of paragraphs: ", list_of_paragraphs(s, t, threshold=None, granule_alignment_funcion=None)
+print "List of paragraphs|bag of words: ", list_of_paragraphs(s, t, threshold=0.8, granule_alignment_funcion=bag_of_words)
+print "List of paragraphs|list of words: ", list_of_paragraphs(s, t, threshold=0.8, granule_alignment_funcion=list_of_words)
+print "Bag of paragraphs: ", bag_of_paragraphs(s, t, threshold=None, granule_alignment_funcion=None)
+print "Bag of paragraphs|bag of words: ", bag_of_paragraphs(s, t, threshold=0.8, granule_alignment_funcion=bag_of_words)
+print "Bag of paragraphs|list of words: ", bag_of_paragraphs(s, t, threshold=0.8, granule_alignment_funcion=list_of_words)
