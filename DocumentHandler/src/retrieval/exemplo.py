abde1561215cf2175f2c3fb2108b01691e14a9f4
Ygor Canalli <ygor.canalli@gmail.com>
2014-12-24 13:00:22 -0200

    list of paragraphs test

--- DocumentHandler/src/retrieval/exemplo.py
+++ DocumentHandler/src/retrieval/exemplo.py
@@ -4,6 +4,7 @@ from extractors_per_dataset.co_derivative_extractor import extract_co_derivative
 from transformers.retrieval_task import RetrievalTask
 import time
 from model.content import Document
+from collections import OrderedDict
 from compare.structural_similarity import *
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from compare.structural_similarity import list_of_paragraphs, bag_of_words
@@ -39,10 +40,10 @@ if __name__ == '__main__':
     #o dataset inteiro 
 #     queries_teste, corpus_index_teste, target_teste = extract_dev_set(queries, corpus_index, target)
     # 2 consultas e seus relevantes
-    queries_teste, corpus_index_teste, target_teste = extract_dev_set(queries, corpus_index, target,2)      
+    queries_teste, corpus_index_teste, target_teste = extract_dev_set(queries, corpus_index, target,3)      
 
     #tokenizando com o sckitlearn (bow)
-        
+       
     cv = CountVectorizer(ngram_range=(1,1)) # extraindo o bow
     index_td_matrix = cv.fit_transform(corpus_index_teste, target_teste) #matrix termo-documento do indice
     queries_td_matrix = cv.transform(queries_teste) #matrix termo-documento das consultas
@@ -50,6 +51,7 @@ if __name__ == '__main__':
     distance = 'cosine'
     jobs = 1
     rt = RetrievalTask(distance,jobs,index_td_matrix)
+    '''
     start_time = time.process_time()
     queries_ranking, queries_similarities = rt.execute_retrieval(queries_td_matrix, target_teste)
     ranking_elapsed_time = time.process_time() - start_time
@@ -64,40 +66,31 @@ if __name__ == '__main__':
 
 
     print("\'Resultados Ygor\'")
-    
+    '''
     '''
     BAG OF WORDS
     '''
     queries_similarities = zeros( (len(queries_teste),len(corpus_index_teste)))
     queries_ranking = []
     ranking_elapsed_time = 0
-    # execucao da comparacao hierarquica do ygor que gerara queries_similarities
+    # execucao da comparacao hierarquica que gerara queries_similarities
+    #calcular queries_ranking , onde queries_ranking[0][0] é a posicao do documento no indice com maior similaridade para consulta 0 (primeira consulta)
     for i in range(0, len(queries_teste)):
         query = Document(queries_teste[i])
-        queries_ranking.append([])
-        queries_ranking_simil = []
+        queries_ranking.append({})
         for j in range(0, len(corpus_index_teste)):
         
             document = Document(corpus_index_teste[j])
     
             start_time = time.process_time()
             # Fill the result map with similaritys
-            simil = bag_of_words(query, document)        
+            simil = list_of_paragraphs(query, document, granule_alignment_funcion=bag_of_words, threshold=0.8)        
             ranking_elapsed_time += time.process_time() - start_time
             
             queries_similarities[i][j] = simil
-            queries_ranking[i].append(j)
-            queries_ranking_simil.append(j)
+            queries_ranking[i][j] = simil
         #ordena a o ranking para i
-        queries_ranking[i] = [k[0] for k in sorted(zip(queries_ranking[i], queries_ranking_simil), key=lambda l: l[1])]
-        print(queries_ranking[i])
-        print(queries_ranking_simil)
-        print("\n")
-    
-    
-    #adaptar para : 
-    #calcular queries_ranking , onde queries_ranking[0][0] é a posicao do documento no indice com maior similaridade para consulta 0 (primeira consulta)  
-    
+        queries_ranking[i] = OrderedDict(sorted(queries_ranking[i].items(), key=lambda t: t[1], reverse=True)) 
     #queries_ranking, queries_similarities = ...
 
     distance = "bag of words"
@@ -106,138 +99,8 @@ if __name__ == '__main__':
     hfm_sep_matrix = rt.highest_false_match_and_separation(queries_ranking, queries_similarities, target_teste)
     
     print("auc(%s) = %4.2f in %d s"%(distance,auc,ranking_elapsed_time))
-        
+    pprint(interpolated_precision)    
     for queryi_index in range (0,hfm_sep_matrix.shape[0]):
         print("query(%d).(HFM|SEP) = (%4.4f | %4.4f) "%(queryi_index,hfm_sep_matrix[queryi_index][0],hfm_sep_matrix[queryi_index][1]))
-    '''
-    LIST OF WORDS
-    '''
-    queries_similarities = zeros( (len(queries_teste),len(corpus_index_teste)))
-    queries_ranking = []
-    ranking_elapsed_time = 0
-    # execucao da comparacao hierarquica do ygor que gerara queries_similarities
-    for i in range(0, len(queries_teste)):
-        query = Document(queries_teste[i])
-        queries_ranking.append([])
-        queries_ranking_simil = []
-        for j in range(0, len(corpus_index_teste)):
         
-            document = Document(corpus_index_teste[j])
     
-            start_time = time.process_time()
-            # Fill the result map with similaritys
-            simil = list_of_words(query, document)
-            ranking_elapsed_time += time.process_time() - start_time       
-            
-            queries_similarities[i][j] = simil
-            queries_ranking[i].append(j)
-            queries_ranking_simil.append(j)
-        #ordena a o ranking para i
-        queries_ranking[i] = [k[0] for k in sorted(zip(queries_ranking[i], queries_ranking_simil), key=lambda l: l[1])]
-        print(queries_ranking[i])
-        print(queries_ranking_simil)
-        print("\n")
-    
-    
-    #adaptar para : 
-    #calcular queries_ranking , onde queries_ranking[0][0] é a posicao do documento no indice com maior similaridade para consulta 0 (primeira consulta)  
-    
-    #queries_ranking, queries_similarities = ...
-
-    distance = "list of words"
-    
-    interpolated_precision, auc = rt.interpolated_precision_recall_curve(queries_ranking, queries_similarities, target_teste)
-    hfm_sep_matrix = rt.highest_false_match_and_separation(queries_ranking, queries_similarities, target_teste)
-    
-    print("auc(%s) = %4.2f in %d s"%(distance,auc,ranking_elapsed_time))
-        
-    for queryi_index in range (0,hfm_sep_matrix.shape[0]):
-        print("query(%d).(HFM|SEP) = (%4.4f | %4.4f) "%(queryi_index,hfm_sep_matrix[queryi_index][0],hfm_sep_matrix[queryi_index][1]))  
-    '''
-    BAG OF PARAGRAPHS / BAG OF WORDS
-    '''
-    queries_similarities = zeros( (len(queries_teste),len(corpus_index_teste)))
-    queries_ranking = []
-    ranking_elapsed_time = 0
-    # execucao da comparacao hierarquica do ygor que gerara queries_similarities
-    for i in range(0, len(queries_teste)):
-        query = Document(queries_teste[i])
-        queries_ranking.append([])
-        queries_ranking_simil = []
-        for j in range(0, len(corpus_index_teste)):
-        
-            document = Document(corpus_index_teste[j])
-    
-            start_time = time.process_time()
-            # Fill the result map with similaritys
-            simil = bag_of_paragraphs(query, document, granule_alignment_funcion=bag_of_words, threshold=0.8)
-            ranking_elapsed_time += time.process_time() - start_time     
-            
-            queries_similarities[i][j] = simil
-            queries_ranking[i].append(j)
-            queries_ranking_simil.append(j)
-        #ordena a o ranking para i
-        queries_ranking[i] = [k[0] for k in sorted(zip(queries_ranking[i], queries_ranking_simil), key=lambda l: l[1])]
-        print(queries_ranking[i])
-        print(queries_ranking_simil)
-        print("\n")
-    
-    
-    #adaptar para : 
-    #calcular queries_ranking , onde queries_ranking[0][0] é a posicao do documento no indice com maior similaridade para consulta 0 (primeira consulta)  
-    
-    #queries_ranking, queries_similarities = ...
-
-    distance = "bag of paragraph / bag of words"
-    
-    interpolated_precision, auc = rt.interpolated_precision_recall_curve(queries_ranking, queries_similarities, target_teste)
-    hfm_sep_matrix = rt.highest_false_match_and_separation(queries_ranking, queries_similarities, target_teste)
-    
-    print("auc(%s) = %4.2f in %d s"%(distance,auc,ranking_elapsed_time))
-        
-    for queryi_index in range (0,hfm_sep_matrix.shape[0]):
-        print("query(%d).(HFM|SEP) = (%4.4f | %4.4f) "%(queryi_index,hfm_sep_matrix[queryi_index][0],hfm_sep_matrix[queryi_index][1])) 
-    '''
-    LIST OF PARAGRALHS / BAG OF WORDS
-    '''    
-    queries_similarities = zeros( (len(queries_teste),len(corpus_index_teste)))
-    queries_ranking = []
-    ranking_elapsed_time = 0
-    # execucao da comparacao hierarquica do ygor que gerara queries_similarities
-    for i in range(0, len(queries_teste)):
-        query = Document(queries_teste[i])
-        queries_ranking.append([])
-        queries_ranking_simil = []
-        for j in range(0, len(corpus_index_teste)):
-        
-            document = Document(corpus_index_teste[j])
-    
-            start_time = time.process_time()
-            # Fill the result map with similaritys
-            simil = list_of_paragraphs(query, document, granule_alignment_funcion=bag_of_words, threshold=0.8)
-            ranking_elapsed_time += time.process_time() - start_time      
-            
-            queries_similarities[i][j] = simil
-            queries_ranking[i].append(j)
-            queries_ranking_simil.append(j)
-        #ordena a o ranking para i
-        queries_ranking[i] = [k[0] for k in sorted(zip(queries_ranking[i], queries_ranking_simil), key=lambda l: l[1])]
-        print(queries_ranking[i])
-        print(queries_ranking_simil)
-        print("\n")
-    
-    
-    #adaptar para : 
-    #calcular queries_ranking , onde queries_ranking[0][0] é a posicao do documento no indice com maior similaridade para consulta 0 (primeira consulta)  
-    
-    #queries_ranking, queries_similarities = ...
-
-    distance = "list of paragraph / bag of words"
-    
-    interpolated_precision, auc = rt.interpolated_precision_recall_curve(queries_ranking, queries_similarities, target_teste)
-    hfm_sep_matrix = rt.highest_false_match_and_separation(queries_ranking, queries_similarities, target_teste)
-    
-    print("auc(%s) = %4.2f in %d s"%(distance,auc,ranking_elapsed_time))
-        
-    for queryi_index in range (0,hfm_sep_matrix.shape[0]):
-        print("query(%d).(HFM|SEP) = (%4.4f | %4.4f) "%(queryi_index,hfm_sep_matrix[queryi_index][0],hfm_sep_matrix[queryi_index][1]))
